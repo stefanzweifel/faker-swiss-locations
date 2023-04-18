5a94ea51eb3bd9b7646d5fc9f9c9b4bf6cb089d5
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-04-18 20:29:04 +0200

    Add more tests

--- tests/LocationTest.php
+++ tests/LocationTest.php
@@ -4,15 +4,44 @@ use Faker\Factory;
 use Wnx\FakerSwissCities\Provider\Location;
 use Wnx\SwissCantons\Canton;
 
-it('returns location instance with a canton instance from the location method', function () {
+beforeEach(function () {
     $faker = Factory::create();
     $faker->addProvider(new Location($faker));
 
+    $this->faker = $faker;
+});
+
+it('returns location instance with a canton instance from the location method', function () {
     /** @var \Wnx\FakerSwissCities\Location $location */
-    $location = $faker->location();
+    $location = $this->faker->location();
 
     expect($location)
         ->toBeInstanceOf(\Wnx\FakerSwissCities\Location::class)
         ->and($location->canton)
+        ->toBeInstanceOf(Canton::class);
+});
+
+it('returns valid swiss postcode', function () {
+    $zipcode = $this->faker->postcode();
+
+    expect($zipcode)
+        ->toBeString()
+        ->and(strlen($zipcode))
+        ->toBe(4);
+});
+
+it('returns valid swiss city', function () {
+    $city = $this->faker->city();
+
+    expect($city)
+        ->toBeString()
+        ->and(strlen($city))
+        ->toBeGreaterThan(0);
+});
+
+it('return valid swiss canton', function () {
+    $canton = $this->faker->canton();
+
+    expect($canton)
         ->toBeInstanceOf(Canton::class);
 });
