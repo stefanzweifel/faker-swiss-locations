0a9d36f8f1c0f6dc4c4bae0440d3920fc29f6a6e
Stefan Zweifel <stefan@stefanzweifel.dev>
2023-04-18 20:28:57 +0200

    Add Code Comments

--- src/Provider/Location.php
+++ src/Provider/Location.php
@@ -3,12 +3,18 @@
 namespace Wnx\FakerSwissCities\Provider;
 
 use Faker\Provider\Base;
+use JsonException;
 use Wnx\SwissCantons\Canton;
 use Wnx\SwissCantons\CantonManager;
+use Wnx\SwissCantons\Exceptions\CantonException;
 use Wnx\SwissCantons\ZipcodeSearch;
 
 class Location extends Base
 {
+    /**
+     * @throws CantonException
+     * @throws JsonException
+     */
     public function location(): \Wnx\FakerSwissCities\Location
     {
         $zipcodeSearch = new ZipcodeSearch();
@@ -26,16 +32,28 @@ class Location extends Base
         );
     }
 
+    /**
+     * @throws CantonException
+     * @throws JsonException
+     */
     public function postcode(): string
     {
         return $this->location()->postcode;
     }
 
+    /**
+     * @throws CantonException
+     * @throws JsonException
+     */
     public function city(): string
     {
         return $this->location()->city;
     }
 
+    /**
+     * @throws CantonException
+     * @throws JsonException
+     */
     public function canton(): Canton
     {
         return $this->location()->canton;
